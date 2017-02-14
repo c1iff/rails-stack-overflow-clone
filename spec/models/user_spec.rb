7d92b25f5436ccfcec304e3631d31f5fffede936
Chris Clifford and Tracie Weitzman <chris@>
2017-02-14 10:10:29 -0800

    add spec for authenticates method

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,12 @@ require 'rails_helper'
 
 describe User do
   it { should validate_presence_of :email }
+  it { should validate_presence_of :password }
+
+  it 'authenticates the current user' do
+    user = User.create(:email => 'user@email.com', :password => 'password123')
+    expect(User.authenticate('user@email.com', 'password123')).to eq(user)
+  end
+
+
 end
