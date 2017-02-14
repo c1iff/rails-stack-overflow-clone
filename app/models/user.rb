844a41f693c1cc5d900eba3dde0bef53037c5baa
Chris Clifford and Tracie Weitzman <tracie@>
2017-02-14 09:31:03 -0800

    add user authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,11 @@ class User < ApplicationRecord
     self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
   end
   def self.authenticate(email, password)
+    user = User.where(email: email).first
+    if user && user.password_hash ==  BCrypt::Engine.hash_secret(password, user.password_salt)
+      user
+    else
+      nil
+    end
   end
 end
