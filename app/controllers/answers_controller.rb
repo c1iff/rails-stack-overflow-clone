36e28f5984194ec5ddde41006e99bc1dded2c099
Chris Clifford and Tracie Weitzman <chris@>
2017-02-14 11:34:24 -0800

    add answer controller and routes

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,7 +1,18 @@
 class AnswersController < ApplicationController
   def new
+    @question = Question.find(params[:question_id])
+    @answer = Answer.new
   end
   def create
+    @question = Question.find(params[:question_id])
+    @answer = Answer.new(answer_params)
+    if @answer.save
+      flash[:notice] = "Answer added"
+      redirect_to question_path(@question)
+    else
+      flash[:alert] = "Please try again"
+      redirect_to :back
+    end
   end
   def update
   end
@@ -9,6 +20,6 @@ class AnswersController < ApplicationController
   end
 private
   def answer_params
-    params.require(:answer).permit(:content, :rating, :user_id)
+    params.require(:answer).permit(:content, :question_id, :user_id)
   end
 end
